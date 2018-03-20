9654b8d80478b0549d579bf06f13d490bfa1f389
teepiik <teemu.piik@gmail.com>
2018-03-20 15:08:25 +0200

    backend started

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# IPAIPA
+# IPAIPA - Beer and drinks application
 Backend and documentation for the IPAIPA beer and drinks application. Code for frontend is located in another repository.
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+let beers = [
+    {
+        id: 1,
+        name: 'Koff',
+        type: 'Lager'
+
+    },
+    {
+        id: 2,
+        name: 'Paulaner',
+        type: 'Wheat beer'
+
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Welcome to IPAIPA</h1>')
+})
+
+app.get('/beers', (req, res) => {
+    res.json(beers)
+})
+
+app.get('/beers/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const beer = beers.find(beer => beer.id === id)
+    if (beer) {
+        response.json(beer)
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
