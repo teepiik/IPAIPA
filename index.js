ece1b88a97acee53f961190cee03c7e122882eda
teepiik <teemu.piik@gmail.com>
2018-04-05 18:56:38 +0300

    uses build

--- index.js
+++ index.js
@@ -7,30 +7,41 @@ const mongoose = require('mongoose')
 const beerRouter = require('./controllers/beerController')
 const config = require('./utils/config')
 
+require('dotenv').config()
+
 
 mongoose
-  .connect(config.mongoUrl)
-  .then( () => {
-    console.log('connected to database', config.mongoUrl)
-  })
-  .catch( err => {
-    console.log(err)
-  })
+    .connect(config.mongoUrl)
+    .then(() => {
+        console.log('connected to database', config.mongoUrl)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api/beers', beerRouter)
-
+mongoose.Promise = global.Promise
 const server = http.createServer(app)
+
 server.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`)
-})
+    console.log(`Server running on port ${config.port}`)
+  })
 
 server.on('close', () => {
   mongoose.connection.close()
 })
 
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(express.static('build'))
+
+
+const error = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(error)
+
 module.exports = {
-  app, server
+    app, server
 }
