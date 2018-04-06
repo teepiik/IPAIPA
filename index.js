789877cf151455b0bda5e628fadb6513f28d57a0
teepiik <teemu.piik@gmail.com>
2018-04-06 15:17:32 +0300

    router fix

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const config = require('./utils/config')
 
 require('dotenv').config()
 
-
 mongoose
     .connect(config.mongoUrl)
     .then(() => {
@@ -24,23 +23,23 @@ const server = http.createServer(app)
 
 server.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`)
-  })
+})
 
 server.on('close', () => {
-  mongoose.connection.close()
+    mongoose.connection.close()
 })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/beers', beerRouter)
 
 app.use(express.static('build'))
 
-
 const error = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(error)
+}
+
+app.use(error)
 
 module.exports = {
     app, server
