150f8b0343a24216134f2490305e6a35f29c022a
teepiik <teemu.piik@gmail.com>
2018-04-12 19:20:05 +0300

    minor fix

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -19,7 +19,7 @@ beerRouter.get('/:id', async (request, response) => {
     try {
         const beer = await Beer.findById(request.params.id)
         if (beer) {
-            response.status(200).json(Beer.format(beer))
+            response.status(200).json(Beer.format)
         } else {
             response.status(404).end()
         }
