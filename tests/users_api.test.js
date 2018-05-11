484fb7a4cbbb89e26c3b645b707cd681bd8340f2
teepiik <teemu.piik@gmail.com>
2018-05-11 12:36:58 +0300

    fixing tests

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -2,6 +2,7 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const User = require('../models/user')
+const { usersInDatabase } = require('./test_helper')
 
 beforeAll(async () => {
     // Use test database!
@@ -12,13 +13,31 @@ beforeAll(async () => {
         passwordHash: "123"
     })
 
+    const newUser2 = new User({
+        username: "Testimies",
+        passwordHash: "321"
+    })
+
     await newUser.save()
+    await newUser2.save()
 })
 
-describe('API GET get user from api/users', async = () => {
-    test(' fake to pass', async = () => {
-        expect(1).toBe(1)
+describe('API GET get user from api/users', async () => {
+    test(' get api/users: users are returned and formatted correctly', async () => {
+        usersInDB = await usersInDatabase()
+
+        const response = await api
+            .get('/api/users')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body.length).toBe(usersInTestDB.length)
     })
+
+    /*
+    usersInDB.forEach(user => {
+        expect(user.passwordHash).toBe(undefined)
+    });*/
 })
 
 afterAll(() => {
