81391cc8cd0836735cfaa3b8619451ddc60b8d39
teepiik <teemu.piik@gmail.com>
2018-06-02 13:05:04 +0300

    username validation unique, userController backend update, delete added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,17 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 const config = require('../utils/config')
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
+
+const getToken = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 // adding users is designed to be handled by admin, no purpose build UI needed.
 // 
@@ -15,6 +24,12 @@ usersRouter.post('/', async (request, response) => {
 
     try {
         const body = request.body
+        const users = await User.find({})
+        const usernames = users.map(user => user.username)
+        if (usernames.includes(body.username)) {
+            return response.status(400).json({ error: 'username must be unique' })
+        }
+
         const saltRounds = 10
         const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
@@ -28,19 +43,19 @@ usersRouter.post('/', async (request, response) => {
 
     } catch (error) {
         console.log(error)
-        response.status(500).json({ error: 'something went wrong' })
+        response.status(500).json({ error: 'internal server error' })
     }
 })
 
 usersRouter.get('/', async (request, response) => {
     try {
         const users = await User.find({})
-        .populate('beers')
+            .populate('beers')
         response.status(200).json(users.map(User.format))
 
     } catch (error) {
         console.log(error)
-        response.status(500).json({ error: 'internal error' })
+        response.status(500).json({ error: 'internal server error' })
     }
 })
 
@@ -51,7 +66,61 @@ usersRouter.get('/:id', async (request, response) => {
 
     } catch (error) {
         console.log(error)
-        response.status(500).json({ error: 'internal error' })
+        response.status(500).json({ error: 'internal server error' })
+    }
+})
+
+// implement that user can only update own account
+usersRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    try {
+        const token = getToken(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+        const userToUpdate = {
+            username: body.username,
+            passwordHash: passwordHash,
+            beersAdded: body.beersAdded,
+            reviews: body.reviews
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(request.params.id, userToUpdate, { new: true })
+        response.status(200).json(User.format(updatedUser))
+
+    } catch (error) {
+        if (error.name === 'JsonWebTokenError') {
+            response.status(401).json({ error: error.message })
+        } else {
+            console.log(error)
+            response.status(500).json({ error: 'internal server error' })
+        }
+    }
+})
+// implement that user can only delete own account
+usersRouter.delete('/:id', async (request, response) => {
+    try {
+        const token = getToken(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+        const deletedUser = await User.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+
+    } catch (error) {
+        if (error.name === 'JsonWebTokenError') {
+            response.status(401).json({ error: error.message })
+        } else {
+            console.log(error)
+            response.status(500).json({ error: 'internal error' })
+        }
     }
 })
 
